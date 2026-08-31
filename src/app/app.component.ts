40f5f2846666b724b56c58b60acfe8608cd1f5b1
Franco <francinnante@gmail.com>
2026-08-31 15:12:01 -0300

    [ADD] header con el logo
    El diseño no trae header. Se arma uno mínimo con el logo del sitio real.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html'
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {}
