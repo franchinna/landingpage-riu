750a34bf8b3e5d8a0665421f47cfeedebeab0cb3
Franco <francinnante@gmail.com>
2026-08-31 16:10:47 -0300

    [ADD] footer con el crédito y el copyright
    Fondo oscuro con el gris del diseño.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
 import { HeroComponent } from './sections/hero/hero.component';
 import { DestinationsComponent } from './sections/destinations/destinations.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HeroComponent, DestinationsComponent],
+  imports: [HeaderComponent, HeroComponent, DestinationsComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
